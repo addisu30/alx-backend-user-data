a353ddb5d459e12642ccdaa775f16f624d19af26
addisu30 <dabaleaddisu21@outlook.com>
2023-08-15 15:02:18 +0300

    updating cokies

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,7 +30,6 @@ elif AUTH_TYPE == "session_db_auth":
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
-
 @app.before_request
 def bef_req():
     """
@@ -39,7 +38,7 @@ def bef_req():
     if auth is None:
         pass
     else:
-        setattr(session_cookie(request), "current_user", auth.current_user(request))
+        setattr(request, "current_user", auth.current_user(request))
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
@@ -47,7 +46,7 @@ def bef_req():
             '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            cookie = auth.session_cookie(request)
+            cookie = auth.Session_cookie(request)
             if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
